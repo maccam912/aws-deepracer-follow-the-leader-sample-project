dc481420610a1ff06dd8c84100afcb4ddfa7e8e7
Matt Koski <maccam912@gmail.com>
2022-05-04 18:05:33 -0500

    debug

--- deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
+++ deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
@@ -231,10 +231,9 @@ class ObjectDetectionNode(Node):
     def circle(self, frame):
         start_time = time.time()
         cimage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cimage = cv2.blur(cimage, (10, 10))
         self.get_logger().info(f"convert to grey time = {time.time() - start_time}")
         start_time = time.time()
-        circles = cv2.HoughCircles(cimage, cv2.HOUGH_GRADIENT, 1, 100)
+        circles = cv2.HoughCircles(cimage, cv2.HOUGH_GRADIENT, 1, 10)
         self.get_logger().info(f"find circles = {time.time() - start_time}")
         start_time = time.time()
         #print(circles)
