dfa2517d0c97dd037f8825297faeb96bedc27df7
Matt Koski <maccam912@gmail.com>
2022-05-04 19:54:10 -0500

    debug

--- deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
+++ deepracer_follow_the_leader_ws/object_detection_pkg/object_detection_pkg/object_detection_node.py
@@ -269,7 +269,6 @@ class ObjectDetectionNode(Node):
                 image = image.transpose((1, 2, 0)) # Get channels last
                 rad, x, y, im = self.circle(image)
                 self.get_logger().info(f"Total circle time = {time.time() - start_time}")
-                self.get_logger().info(f"circ = {circ}")
                 display_image = self.bridge.cv2_to_imgmsg(np.array(im), "bgr8")
                 self.display_image_publisher.publish(display_image)
 
